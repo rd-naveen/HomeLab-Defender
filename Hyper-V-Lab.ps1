10bc2cd9d35a3be742c75e08fb6e27f9bfe62b57
Naveen <naveen.sec.pro@gmail.com>
2025-01-25 16:52:26 +0530

    add port mirring commands

--- Hyper-V-Lab.ps1
+++ Hyper-V-Lab.ps1
@@ -44,7 +44,6 @@ function CreateAndConfigureVM {
     )
     # Define the iso file locations and VM files location
     $virtual_harddisk_location = "D:\VMs"
-    $iso_image_dir = "E:\OS Images\"
     
     # Define the interface and LAN names
     $corp_switch_name = "Corp LAN (internal)"
@@ -59,12 +58,21 @@ function CreateAndConfigureVM {
     
     mkdir $virtual_hdd_loc 
 
-    $vm_ = New-VM -Name $vm_name -MemoryStartupBytes $RamSize -NewVHDPath $virtual_hdd_loc\$vm_name.vhdx -NewVHDSizeBytes $StorageSize  -Generation 2 
+    New-VM -Name $vm_name -MemoryStartupBytes $RamSize -NewVHDPath $virtual_hdd_loc\$vm_name.vhdx -NewVHDSizeBytes $StorageSize  -Generation 2 
 
     write-host $VmName + ": Renaming default interface name"
     # #By default we'll have one network adapter, we are going to rename it and connect it to networks
-    Rename-VMNetworkAdapter -VMName $vm_name -Name "Network Adapter" -NewName $corp_interface_name
-    Connect-VMNetworkAdapter -VMName $vm_name -SwitchName $corp_switch_name -Name $corp_interface_name
+    if ($vm_name -eq "Lab-Ubuntu-Sensor"){
+        Rename-VMNetworkAdapter -VMName $vm_name -Name "Network Adapter" -NewName "Monitor"
+        Connect-VMNetworkAdapter -VMName $vm_name -SwitchName $corp_switch_name -Name "Monitor"
+        Set-VMNetworkAdapter -PortMirroring Destination -VMName $vm_name -Name "Monitor"
+    }
+    else{
+        Rename-VMNetworkAdapter -VMName $vm_name -Name "Network Adapter" -NewName $corp_interface_name
+        Connect-VMNetworkAdapter -VMName $vm_name -SwitchName $corp_switch_name -Name $corp_interface_name
+        Set-VMNetworkAdapter -PortMirroring Source -VMName $vm_name -Name "Monitor"
+    }
+    
     
     if ( $InterfaceCount -eq "2"){
         write-host $VmName + ": Adding secondary interface "
@@ -92,7 +100,6 @@ function CreateAndConfigureVM {
         write-host $VmName + ": Disabling Secure Boot for "+ $Category
         Set-VMFirmware $vm_name -EnableSecureBoot Off -FirstBootDevice $dvd
     }
-   
 }
 
 #check if hyper-v feature is enabled or not
@@ -113,9 +120,11 @@ if ((Get-VMSwitch|Where-Object -Property Name  -Like $corp_switch_name).count){
 #create a virtual network
 
 New-VMSwitch -name $corp_switch_name -SwitchType Private
+Enable-VMSwitchExtension -VMSwitchName $corp_switch_name -Name "Microsoft NDIS Capture"
 
 
 #collect the iso images for VMs
+$iso_image_dir = "E:\OS Images\"
 $iso_files = Get-ChildItem $iso_image_dir | Where-Object -Property Extension -EQ .iso | Select-Object Extension, FullName, @{L = 'Length(Gbs)'; E = { [Math]::Round($_.Length / 1024 / 1024 / 1024, 2) } }
     
 $win11_iso = ($iso_files | Where-Object -Property FullName -Match "CLIENTENTERPRISEEVAL").FullName
@@ -134,6 +143,8 @@ $gb_80 = 80*1024*1024*1024
 CreateAndConfigureVM -VmName "Lab-Win11"  -category "windows"  -IsoFileLocation $win11_iso   -InterfaceCount "2"  -RamSize $gb_4   -StorageSize $gb_80  
 createandconfigurevm -vmname "Lab-AD20xx"  -category "windows"  -isofilelocation $winad20xx_iso   -interfacecount "2"  -ramsize $gb_4   -storagesize $gb_80  
 createandconfigurevm -vmname "Lab-Kali-Linux"   -category "linux"   -isofilelocation $kalilinux_iso    -interfacecount "2"   -ramsize $gb_4    -storagesize $gb_80  
-createandconfigurevm -vmname "Lab-Ubuntu"  -category "linux"  -isofilelocation $ubuntu_iso   -interfacecount "2"  -ramsize $gb_4   -storagesize $gb_80 
+createandconfigurevm -vmname "Lab-Ubuntu-Sensor"  -category "linux"  -isofilelocation $ubuntu_iso   -interfacecount "2"  -ramsize $gb_4   -storagesize $gb_80 
 
 
+# TODO:
+    # make the ubuntu server to listen all the traffic comming and going from all VMs, inside the Corp Lan network.
